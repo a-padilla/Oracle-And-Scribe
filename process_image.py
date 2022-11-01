028d8a52af6cbfa43ac412c6626825643ef03ce6
bradleyjohnsxn <62025141+bradleyjohnsxn@users.noreply.github.com>
2022-11-01 14:02:24 -0400

    fixed file path

---
+++ process_image.py
@@ -0,0 +1,24 @@
+# import libraries
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+import numpy as np
+import os
+
+# get text from an image as a string
+text_from_image = pytesseract.image_to_string(Image.open('images/test1.jpg'))
+
+# separate string into lines
+def strippy(s):
+    return s.strip()
+
+lines = text_from_image.split('\n')
+clean = np.array(lines)
+stripper = np.vectorize(strippy)
+clean = stripper(clean)
+clean = clean[clean!='']
+
+for i in range(len(clean)):
+    print(clean[i])
