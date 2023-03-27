a093039cb1fb01f5929757e99c2ffead5291ecbb
khmarin.anthony@gmail.com <khmarin.anthony@gmail.com>
2023-03-27 14:07:06 -0400

    restructure folders

--- Alice/send_to_scribe.py
+++ Alice/send_to_scribe.py
@@ -1,26 +1,16 @@
-import time
-import signal
-from PCClient import PCClient
+from Alice import Alice
 from bluetooth import *
-
-exit_ = False
-def sigint_handler(signum, frame):
-    global exit_
-    exit_ = True
-
-signal.signal(signal.SIGINT, sigint_handler)
-signal.signal(signal.SIGTERM, sigint_handler)
 
 if __name__ == "__main__":
 	is_connected = False
-	pcclient = PCClient()
+	alice = Alice()
 
 	try:
-		pcclient.connect()
+		alice.connect()
 		is_connected = True
 	except:
 		while not is_connected:
-			if not pcclient.retry_connection():
+			if not alice.retry_connection():
 				print("Connection error")
 			else:
 				print("Connected")
@@ -31,21 +21,21 @@ if __name__ == "__main__":
 
 		key = input("Press 'q' to quit, 'c' to capture, 'b' to reconnect: ")
 		print(key)
-		if key == 'q' and pcclient != None:
+		if key == 'q' and alice != None:
 			exit_ = True
-		if key == 'c' and pcclient != None:
+		if key == 'c' and alice != None:
 			try:
-				lines = pcclient.get_text()
+				lines = alice.get_text()
 				print(lines)
-				if pcclient.send_lines(lines):
+				if alice.send_lines(lines):
 					print('sent')
 			except:
 				print("Something went wrong. Retry connecting to bluetooth")
 			
 		if key == 'b':
 			print("Attempting to reconnect")
-			if pcclient.retry_connection():
+			if alice.retry_connection():
 				print("Success")
 			else:
 				print("Failed")
-	pcclient.close()
+	alice.close()
