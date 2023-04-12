243e8bd1f59b9ec1973129583fdfee0ddc9b50c5
khmarin.anthony@gmail.com <khmarin.anthony@gmail.com>
2023-04-12 19:26:55 -0400

    change to gpio

--- Oracle/Oracle_new.py
+++ Oracle/Oracle_new.py
@@ -4,7 +4,7 @@ import mediapipe as mp
 import pytesseract
 import bluetooth
 import time
-
+import RPi.GPIO as GPIO
 
 # Initialize mediapipe and Tesseract OCR
 handsModule = mp.solutions.hands.Hands(static_image_mode=False, max_num_hands=2,
@@ -52,36 +52,33 @@ def process_gesture(frame):
 
 
 # Define a function to perform OCR on the hand gesture
-def perform_ocr(gesture, frame):
+def perform_ocr(frame):
     # gesture = gesture_future.result()
-    if gesture != None:
-        print("OCR got gesture: " + gesture)
     text = None
-    if gesture != None:
-        # Convert the hand gesture to a grayscale image
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Convert the hand gesture to a grayscale image
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # Apply binary thresholding
-        # thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    # Apply binary thresholding
+    # thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
 
-        # # Apply median filtering to remove noise
-        # filtered = cv2.medianBlur(thresh, 3)
+    # # Apply median filtering to remove noise
+    # filtered = cv2.medianBlur(thresh, 3)
 
-        # # Deskew the image
-        # coords = np.column_stack(np.where(filtered > 0))
-        # angle = cv2.minAreaRect(coords)[-1]
-        # if angle < -45:
-        #     angle = -(90 + angle)
-        # else:
-        #     angle = -angle
-        # (h, w) = frame.shape[:2]
-        # center = (w // 2, h // 2)
-        # M = cv2.getRotationMatrix2D(center, angle, 1.0)
-        # rotated = cv2.warpAffine(filtered, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    # # Deskew the image
+    # coords = np.column_stack(np.where(filtered > 0))
+    # angle = cv2.minAreaRect(coords)[-1]
+    # if angle < -45:
+    #     angle = -(90 + angle)
+    # else:
+    #     angle = -angle
+    # (h, w) = frame.shape[:2]
+    # center = (w // 2, h // 2)
+    # M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    # rotated = cv2.warpAffine(filtered, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
 
-        # Perform text recognition on the preprocessed image
-        text = pytesseract.image_to_string(gray)
-        print(text)
+    # Perform text recognition on the preprocessed image
+    text = pytesseract.image_to_string(gray)
+    print(text)
 
     return text
 
@@ -133,19 +130,19 @@ def main():
     picam2.configure(config)
     picam2.start()
 
-
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(2, GPIO.IN)
     print("Starting Main loop")
     # Loop to capture frames and put them in the queue
     while True:
         # Capture a frame from the camera
-        frame = picam2.capture_array()
+        
         print("New frame")
 
-        # Put the frame in the queue
-        gesture = process_gesture(frame)
-        print("done with gesture")
-        if gesture == "start":
-            text = perform_ocr(gesture, frame)
+        if GPIO.input(2) == GPIO.HIGH:
+            frame = picam2.capture_array()
+            print("Captured frame")
+            text = perform_ocr(frame)
             send_text(text, sock)
 
         
@@ -154,7 +151,6 @@ def main():
         #Below states that if the |q| is press on the keyboard it will stop the system
         if key == ord("q"):
             break
-        time.sleep(3)
         
     
     picam2.release()
